f0685c09e26fbd3b0350a4a5fd7a145c3300908f
appleobject <52360868+appleobject@users.noreply.github.com>
2020-09-24 12:27:05 +0100

    Type: feat
    Add start the Phone activity.

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -42,4 +42,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent actionDial = new Intent(Intent.ACTION_DIAL);
+        actionDial.setData(uri);
+        startActivity(actionDial);
+    }
 }
